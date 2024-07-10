017680677ceb5c99806edd52d4cd14623ff6e88b
Makar <makartemlyakov@gmail.com>
2024-07-11 00:23:46 +0300

    fix radio option

--- main.js
+++ main.js
@@ -23,32 +23,38 @@ document.querySelector('#app').innerHTML = `
         <p class="access-list__text">${tr.magicAvatarsWith20Off}</p>
       </li>
     </ul>
-   <ul class="option">
-    <li class="option-item--active">
-      <div class="per-year-wrapper">
+   <form class="option">
+    <div class="form_radio_btn">
+      <input id="radio-1" type="radio" name="radio" value="1" checked>
+      <label for="radio-1">
+        <div class="per-year-wrapper">
         <h2 class="option-item__title">${tr.yarlyAccess}</h2>
-        <p classs="option-item__per-year">${tr.justPricePerYear.replace(
+          <p classs="option-item__per-year">${tr.justPricePerYear.replace(
+            '{{price}}',
+            getCurrency(39.99),
+          )}</p>
+        </div>
+        <p class="option-item__week-label">${tr.pricePerWeek.replace(
           '{{price}}',
-          getCurrency(39.99),
+          getCurrency(0.48),
         )}</p>
-      </div>
-      <p class="option-item__week-label">${tr.pricePerWeek.replace(
-        '{{price}}',
-        getCurrency(0.48),
-      )}</p>
-      <span class="option-item__best-offer">${tr.bestOffer}</span>
-    </li>
-    <li class="option-item">
+        <span class="option-item__best-offer">${tr.bestOffer}</span>
+    </div>
+    </label>
+    <div class="form_radio_btn">
+      <input id="radio-2" type="radio" name="radio" value="2">
+      <label for="radio-2">
       <div class="per-year-wrapper">
       <h2 class="option-item__title">${tr.weaklyAccess}</h2>
       </div>
-      <p class="option-item__week-label">${tr.pricePerWeek.replace(
-        '{{price}}',
-        getCurrency(6.99),
-      )}</p>
-    </li>
-   </ul>
-   <button class="button">${tr.continue}</button>
+        <p class="option-item__week-label">${tr.pricePerWeek.replace(
+          '{{price}}',
+          getCurrency(0.48),
+        )}</p>
+      </label>
+    </div>
+    <button class="button">${tr.continue}</button>
+   </form>
    <footer class="footer">
    <nav class="navigation">
     <ul class="navigation-list">
@@ -65,3 +71,19 @@ document.querySelector('#app').innerHTML = `
    </div>
   </div>
 `;
+
+const form = document.querySelector('form[class=option]');
+const button = form.querySelector('.button');
+
+button.addEventListener('click', () => {
+  const optionValue = form.querySelector('input[name=radio]:checked')?.value;
+  event.preventDefault();
+  switch (optionValue) {
+    case '1':
+      window.location.replace('https://apple.com/');
+      break;
+    case '2':
+      window.location.replace('https://google.com/');
+      break;
+  }
+});
