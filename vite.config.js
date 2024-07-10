4efe7efddfe7bafd3fce275027d85e6eb3cd931c
Makar <makartemlyakov@gmail.com>
2024-07-10 21:58:18 +0300

    change config vite

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
-
-import {defineConfig} from 'vite';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
   esbuild: {
     supported: {
-      'top-level-await': true //browsers can handle top-level-await features
+      'top-level-await': true, //browsers can handle top-level-await features
     },
-  }
-})
+  },
+  base: '',
+});
